ac0efb5f2ff53c7747f7f2636fdd59a2b7d79a24
Sarah Aguasvivas <31457827+sarahaguasvivas@users.noreply.github.com>
2019-06-07 20:17:50 -0600

    Add files via upload

---
+++ src/pwong_gym.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+import gym
+env = gym.make("Pong-v0")
+observation = env.reset()
+for _ in range(1000):
+  env.render()
+  action = env.action_space.sample() # your agent here (this takes random actions)
+  observation, reward, done, info = env.step(action)
+
+  if done:
+    observation = env.reset()
+env.close()
